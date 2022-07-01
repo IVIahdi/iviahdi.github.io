50f2e0420a53c85fbb2d19ffb2ace5fb8a60c23c
IVIahdi <84638055+IVIahdi@users.noreply.github.com>
2022-07-02 02:05:59 +0300

    123

--- home-page.js
+++ home-page.js
@@ -5,11 +5,3 @@ function logo_clicked() {
     logo.classList.toggle("animate__heartBeat")
     content.classList.toggle("fade")
 }
-function add(){
-    var table = document.getElementById('table')
-    var row = table.insertRow()
-    var cell = row.insertCell().innerHTML = '<td><input type="text"></td>'
-    var select = ''
-    cell = row.insertCell().innerHTML = '<td><input type="text"></td>'
-    cell = row.insertCell().innerHTML = '<td><input type="text"></td>'
-}
