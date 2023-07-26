b9f29dce1aa15ae10d1cfd3adba0a039d3b1fb87
IVIahdi <mahdi.11231@gmail.com>
2023-07-27 02:31:41 +0300

    major updates

--- README.md
+++
@@ -1,3 +0,0 @@
-# iviahdi.github.io
-This is a simple wbsite
-I made this for KFUPM Open source community
--- home-page.js
+++
@@ -1,7 +0,0 @@
-function logo_clicked() {
-    let logo = document.getElementById('logo')
-    let content = document.getElementById('big-content')
-    logo.classList.toggle("fade")
-    logo.classList.toggle("animate__heartBeat")
-    content.classList.toggle("fade")
-}
